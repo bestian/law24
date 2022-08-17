40e9b18f2a13318fad6d9b0b1e59271f24bcba0b
Bestian Tang 小巴 <bestian@gmail.com>
2022-08-18 07:46:50 +0800

    test

--- content.js
+++ content.js
@@ -1,3 +1,7 @@
-var h = document.body.innerHTML;
-h = h.replace(/唐宗浩/g/, '路人甲')
-document.body.innerHTML = h;
+$(document).ready(function () {
+	var h = document.body.innerHTML;
+	h = h.replace(/唐宗浩/g, '路人甲');
+	h = h.replace(/唐鳳胞弟/g, '路人甲');
+	document.body.innerHTML = h;
+})
+
